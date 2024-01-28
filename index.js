5f26b8161666aaac30b4fa75678c47ca9ca1be25
Rodwell <matchon.rodwell@gmail.com>
2024-01-29 00:00:39 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const port =  8000;
+
+app.get('/', (req, res) => {
+    res.send('<div>Hello, World!</div>');
+    console.log("request received")
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
