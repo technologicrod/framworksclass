7881f89b5ac5d29803971dfeb3637baaad07f28b
Rodwell <matchon.rodwell@gmail.com>
2024-03-06 01:39:46 +0800

    added express server

--- oldindex.js
+++ oldindex.js
@@ -2,12 +2,33 @@ const express = require('express')
 const app = express()
 
 const port =  8000;
+
+app.use((req,res, next) => {
+    console.log("Request made")
+    console.log(`Host: ${req.hostname}`)
+    console.log(`Path: ${req.path}`)
+    console.log(`Method>: ${req.method}`)
+    next();
+})
 app.get('/', (req, res) => {
-    res.send('<div>Hello, World! <h1>HAHA</h1></div>');
-    console.log("request received")
+    res.sendFile('./views/index.html', { root: __dirname})
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname})
+});
 
+app.get('/home',  (req, res) =>{
+    res.redirect('/')
+})
+
+app.get('/aboutus',  (req, res) =>{
+    res.redirect('/about')
+})
+
+app.use((req, res) =>{
+    res.status(404).sendFile('./views/error.html', { root: __dirname })
+})
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
