cfeb0269ce357a70affe5d60f433b941c5faabfe
Rodwell <matchon.rodwell@gmail.com>
2024-04-03 01:11:54 +0800

    added partials and weather npm

--- oldindex.js
+++ oldindex.js
@@ -3,6 +3,28 @@ const app = express()
 const { students, hello } = require('./mymodule')
 app.set('view engine', 'ejs')
 const port =  8000;
+const weather = require('weather-js')
+weather.find({ search: 'Manila, Philippines', degreeType: 'C' }, function(err, result) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(JSON.stringify(result, null ,2))
+    // Result will be an array of weather data
+    if (result && result.length > 0) {
+        const location = result[0].location;
+        const current = result[0].current;
+
+        console.log(`Weather in ${location.name}`);
+        console.log(`- Condition: ${current.skytext}`);
+        console.log(`- Temperature: ${current.temperature}°C`);
+        console.log(`- Feels Like: ${current.feelslike}°C`);
+        console.log(`- Humidity: ${current.humidity}%`);
+        console.log(`- Wind: ${current.winddisplay}`);
+    } else {
+        console.log('Weather information not found for Manila.');
+    }
+});
 
 //console.log(students);
 app.use((req,res, next) => {
@@ -11,6 +33,19 @@ app.use((req,res, next) => {
     console.log(`Path: ${req.path}`)
     console.log(`Method>: ${req.method}`)
     next();
+})
+app.get('/davao', (req, res) =>{
+    weather.find({search: 'Davao, PH', degreeType: 'C'}, function(err, result) {
+        if(err) {console.log(err);}
+        else{
+            let data = {
+                url: req.url,
+                weatherdavao: eval(JSON.stringify(result, null, 2))
+            }
+            res.render('davao', data);
+        }
+       
+      });
 })
 app.get('/', (req, res) => {
     res.render('index', {heading: "ex battalion music"})
